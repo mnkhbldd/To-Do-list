cf0ae0de8641ff2914bc0ef32b93ab4d9db5d36c
mnkhbldd <136554405+mnkhbldd@users.noreply.github.com>
2025-02-18 12:23:01 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,46 @@
+"use client";
 import { Button, Container, Task } from "@/components";
-import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const mockData = [
+    {
+      title: "Bla blabla",
+      isChecked: false,
+    },
+    {
+      title: "Bla blabla",
+      isChecked: false,
+    },
+  ];
+
+  const [test, setTest] = useState(mockData);
+
+  const addData = () => {
+    const newData = [...test, { title: "test", isChecked: false }];
+    setTest(newData);
+    console.log(newData);
+  };
+
   return (
     <div className="flex flex-col w-[345px] h-fit bg-white  items-center gap-5 px-[16px] py-6">
       <p className="text-black font-semibold text-[20px]">To-Do List</p>
       <div className="flex gap-[6px]">
         <Container />
-        <Button className={""}>Add</Button>
+        <Button className={""} onClick={addData}>
+          Add
+        </Button>
       </div>
       <div className="flex gap-[6px] w-full">
         <Button className={"px-3 py-1 bg-[#cacaca] text-black"}>All</Button>
         <Button className={"px-3 py-1 bg-[#cacaca] text-black"}>Active</Button>
         <Button className={"px-3 py-1 "}>Completed</Button>
       </div>
-      <Task />
-      <Task />
+      {test.map((value, index) => {
+        return (
+          <Task key={index} Title={value.title} isDone={value.isChecked}></Task>
+        );
+      })}
       <div className="flex flex-col gap-10 w-full items-center">
         {" "}
         <div className="flex items-center justify-between w-full pt-4 pb-1 border-t ">
