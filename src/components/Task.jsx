c3be2e5b698ca522da62e6a0a3be8a58d63ed7a5
mnkhbldd <136554405+mnkhbldd@users.noreply.github.com>
2025-02-18 12:23:20 +0800

    Update Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,25 @@
+"use client";
 import { Button } from ".";
+import { useState } from "react";
 
-export const Task = () => {
+export const Task = ({ Title, isDone }) => {
+  const [isChecked, setIsChecked] = useState(isDone);
+
+  const HandleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div className="flex items-center justify-between w-full p-4 bg-[#cccccc] rounded-md">
       <div className="flex gap-[10px]">
-        <input type="checkbox" />
-        <p className="text-black font-normal">Create PR</p>
+        <input type="checkbox" checked={isChecked} onChange={HandleOnChange} />
+        <p
+          className={`text-black font-normal ${
+            isChecked ? "line-through" : ""
+          }`}
+        >
+          {Title}
+        </p>
       </div>
       <Button className="bg-[#FEF2F2] text-[#EF4444] px-3 py-[6px]">
         Delete
